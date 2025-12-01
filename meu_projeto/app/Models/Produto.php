3f5fdfea5a7216027f5284ca9ffecb70893069dc
Gabzxd <gabrielcoimbra769@gmail.com>
2025-11-30 23:30:45 -0300

    final

--- meu_projeto/app/Models/Produto.php
+++ meu_projeto/app/Models/Produto.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $fillable = ['nome', 'preco', 'categoria_id', 'imagem_path'];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
